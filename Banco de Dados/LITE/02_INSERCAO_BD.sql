3826037a56142f62f75aed8be906c997d5541ea5
Gabriela Pires Da Silva <33938222840@info.local>
2019-03-12 09:39:45 -0300

    atualizando pasta + BD

---
+++ Banco de Dados/LITE/02_INSERCAO_BD.sql
@@ -0,0 +1,63 @@
+INSERT INTO CLINICAS
+VALUES
+	('Vida e Saúde' , 'Rua Barão de Limeira, 539' , '03125120' , '1155674322' , 'SP Medical Group LTDA' , '38343987000101')
+	,
+	('Bem Viver' , 'Rua Tobias Barreto, 2200' , '03134130' , '1155694330' , 'SP Medical Group LTDA' , '38343987000102'); 
+
+
+INSERT INTO ESPECIALIDADES
+VALUES
+	('Clínica Geral')
+	,
+	('Oftalmologia')
+	,
+	('Otorrinlaringologia')
+	,
+	('Pediatria')
+	,
+	('Geriatria');
+
+INSERT INTO USUARIOS_TIPOS
+VALUES
+	('ADMINISTRADOR')
+	,
+	('COMUM')
+	
+
+INSERT INTO STATUS_CONSULTAS
+VALUES
+	('Confirmada')
+	,
+	('Cancelada')
+	,
+	('Finalizada')
+	,
+	('Retorno');
+
+INSERT INTO USUARIOS
+VALUES
+
+	('admin@admin.com', '12345','1')
+	,
+	('carlos@email.com', '12345','2')
+	,
+	('marilucia@email.com', '1236','2');
+
+INSERT INTO MEDICOS
+VALUES
+	('Maria Lucia','6', '12345678901','1');
+
+INSERT INTO PRONTUARIOS
+VALUES
+	('1,60','50Kg', 'Não tomou vacina contra febre amarela');
+
+INSERT INTO PACIENTES
+VALUES
+	('Carlos Roberto', '5', '402345670', '33845634520' , '12-04-2010' , 'Rua Prestes, 500', '03231100' , '1155636512' , '1');
+
+INSERT INTO CONSULTAS
+VALUES
+	('3','4','20-02-2019', '12:00','1', 'Precisa de atendimento com urgência')
+
+
+	
