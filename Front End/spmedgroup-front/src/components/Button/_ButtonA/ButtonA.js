874d2c5c8ff12aacf9e50baa891ed93bb25af2dc
Gabriela Pires Da Silva <33938222840@info.local>
2019-04-22 11:25:17 -0300

    projeto react iniciado

---
+++ Front End/spmedgroup-front/src/components/Button/_ButtonA/ButtonA.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+function ButtonA() {
+  return(
+    <a href="#" className="btn btn--a">Entrar</a>
+      
+);
+}
+
+export default ButtonA;
---
+++ Front End/spmedgroup-front/src/components/Button/_ButtonB/ButtonB.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+function ButtonB() {
+  return(
+    <a href="#" className="btn btn--b">Agendar</a>      
+);
+}
+
+export default ButtonB;
