29cc15e55e255ff1bf12ed4b13f47a99b6869b9c
Gabriela Pires Da Silva <33938222840@info.local>
2019-06-13 11:16:13 -0300

    adicionada documentaçao via swagger na api

--- API/SpMedGroup.WebApi/Startup.cs
+++ API/SpMedGroup.WebApi/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
 
 namespace SpMedGroup.WebApi
 {
@@ -35,6 +30,13 @@ namespace SpMedGroup.WebApi
                         .AllowAnyHeader()
                         .AllowCredentials());
             });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "SpMedGroup API - Gabi", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,8 +51,21 @@ namespace SpMedGroup.WebApi
                 app.UseHsts();
             }
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SpMedGroup API - Gabi");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseCors("CorsPolicy");
+
             app.UseHttpsRedirection();
+
             app.UseMvc();
         }
     }
